46fc7f10cf170b9cc54ec97aeed0184686da156b
Pierre-Luc Loyer <pierreluc.loyer@unity3d.com>
2025-12-28 12:37:08 -0500

    Skill names

---
+++ SkillNames.cs
@@ -0,0 +1,77 @@
+// Auto-generated file - Do not edit manually
+// Generated on: 2025-12-28
+// Total skills: 28
+// Source: C:\Program Files (x86)\Steam\steamapps\common\Knights of Honor II\Defs\Characters\Skills
+
+namespace AIOverhaul
+{
+    /// <summary>
+    /// Constants for all skill names in Knights of Honor II
+    /// </summary>
+    public static class SkillNames
+    {
+        public const string Archery = "Archery";
+        public const string Assassination = "Assassination";
+        public const string Bargain = "Bargain";
+        public const string Bribery = "Bribery";
+        public const string CavalryTactics = "CavalryTactics";
+        public const string Charity = "Charity";
+        public const string Concealment = "Concealment";
+        public const string Conspiracy = "Conspiracy";
+        public const string Construction = "Construction";
+        public const string Courtesy = "Courtesy";
+        public const string FieldTactics = "FieldTactics";
+        public const string InfantryTactics = "InfantryTactics";
+        public const string IronFist = "IronFist";
+        public const string Laws = "Laws";
+        public const string Leadership = "Leadership";
+        public const string Learning = "Learning";
+        public const string Logistics = "Logistics";
+        public const string Medicine = "Medicine";
+        public const string Navigation = "Navigation";
+        public const string Negotiation = "Negotiation";
+        public const string Plotting = "Plotting";
+        public const string Rhetoric = "Rhetoric";
+        public const string Siegecraft = "Siegecraft";
+        public const string Stewardship = "Stewardship";
+        public const string Strategy = "Strategy";
+        public const string Theology = "Theology";
+        public const string Trading = "Trading";
+        public const string Writing = "Writing";
+
+        /// <summary>
+        /// Get all skill names as an array
+        /// </summary>
+        public static readonly string[] All = new[]
+        {
+            Archery,
+            Assassination,
+            Bargain,
+            Bribery,
+            CavalryTactics,
+            Charity,
+            Concealment,
+            Conspiracy,
+            Construction,
+            Courtesy,
+            FieldTactics,
+            InfantryTactics,
+            IronFist,
+            Laws,
+            Leadership,
+            Learning,
+            Logistics,
+            Medicine,
+            Navigation,
+            Negotiation,
+            Plotting,
+            Rhetoric,
+            Siegecraft,
+            Stewardship,
+            Strategy,
+            Theology,
+            Trading,
+            Writing
+        };
+    }
+}
